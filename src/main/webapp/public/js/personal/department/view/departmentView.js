8385412c19ca7ba9547896123cbd4a3bbf8a57e7
skorpion <savinov_vu@inbox.ru>
2017-02-10 21:19:43 +0800

    refactor:  refactor department with ajax url api.

--- src/main/webapp/public/js/personal/department/view/departmentView.js
+++ src/main/webapp/public/js/personal/department/view/departmentView.js
@@ -32,3 +32,13 @@ function view(data) {
     });
 
 }
+
+function getModal(id) {
+    document.getElementById('id').value = id;
+    document.getElementById('name').value = "";
+}
+
+function getDelModal(id, name) {
+    document.getElementById('delId').value = id;
+    document.getElementById('delName').value = ($('#name-'+id).text());
+}
