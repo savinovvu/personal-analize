5db75d8e7984e90273a87840048df43bf5abf8a2
skorpion <savinov_vu@inbox.ru>
2017-02-10 11:47:55 +0800

    refactor: add ajax api.

---
+++ src/main/webapp/public/js/common/ajax/ajax.js
@@ -0,0 +1,48 @@
+
+
+var ajaxAPI = {
+
+    "constructor" : {
+        "answerKit": "/constructor/answerKit",
+        "answerKitAll": "/constructor/answerKit/all",
+        "answerVar": "/constructor/answerVar",
+        "questionKit": "/constructor/questionKit",
+        "questionKitAll": "/constructor/questionKit/all",
+        "questionVar": "/constructor/questionVar"
+    },
+    "personal": {
+        "department" : "/personal/department",
+        "departmentAll": "/personal/department/all",
+        "group": "/personal/group",
+        "person": "/personal/person",
+        "personAll": "/personal/person/all",
+    }
+
+
+
+
+};
+
+
+
+function send(url, type, jsonData, viewFunction) {
+
+    $.ajax({
+
+        url: url,
+        type: type,
+        contentType: 'application/json',
+        data: JSON.stringify(jsonData),
+        success: function (data) {
+
+            viewFunction(data);
+
+        },
+        error: function (x) {
+            alert("error");
+
+        }
+
+    });
+    return false;
+}
