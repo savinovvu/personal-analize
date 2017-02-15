36937bba3652afebffcc0c8cd25082066724834d
skorpion <savinov_vu@inbox.ru>
2017-02-15 16:16:25 +0800

    add QuestionSub for sub Question.

--- src/main/resources/db/scriptsSQL/personal/populateDB.sql
+++ src/main/resources/db/scriptsSQL/personal/populateDB.sql
@@ -3,27 +3,27 @@ DELETE FROM groups;
 DELETE FROM persons;
 
 
-INSERT INTO department (name) VALUES
-  ('department1'),
-  ('department2'),
-  ('Департамент3');
+INSERT INTO department (id, name) VALUES
+  (1,'department1'),
+  (2,'department2'),
+  (3,'Департамент3');
 
 
 
-INSERT INTO groups (name, department_id) VALUES
-  ('otdel1',1),
-  ('otdel2',2),
-  ('Отдел3',3),
-  ('Отдел4',3);
+INSERT INTO groups (id, name, department_id) VALUES
+  (1,'otdel1',1),
+  (2,'otdel2',2),
+  (3,'Отдел3',3),
+  (4,'Отдел4',3);
 
 
-INSERT INTO persons (name, group_id) VALUES
-  ('name1', 4),
-  ('name2', 5),
-  ('name3', 5),
-  ('name4', 6),
-  ('Имя4', 7),
-  ('name4', 7);
+INSERT INTO persons (id, name, group_id) VALUES
+  (1,'name1', 1),
+  (2,'name2', 2),
+  (3,'name3', 2),
+  (4,'name4', 3),
+  (5,'Имя4', 4),
+  (6,'name4', 4);
 
 
 
