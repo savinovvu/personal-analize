ac94612275f85ef441cb783028244ef4ef075baf
skorpion <savinov_vu@inbox.ru>
2017-02-23 01:29:16 +0800

    Replace <form> to <div>. Add select "нет ответа" by default.

--- src/main/webapp/public/js/survey/view/view.js
+++ src/main/webapp/public/js/survey/view/view.js
@@ -45,10 +45,9 @@ function viewQuestionSub(data) {
 }
 
 
-var i = 0;
 function viewAnswer(data, question) {
 
-    viewForm(question, question.prefix);
+    viewAnswerDiv(question, question.prefix);
     switch (question.answerKit.answerType) {
         case "CHECKBOX" :
             viewAnswerCheckbox(data, question.id, question.prefix);
@@ -71,8 +70,8 @@ function viewAnswer(data, question) {
     }
 }
 
-function viewForm(question, prefixId) {
-    var output = "<div id='form" + prefixId + question.id + "'></div>";
+function viewAnswerDiv(question, prefixId) {
+    var output = "<div id='answerDiv" + prefixId + question.id + "'></div>";
     $("#" + prefixId + question.id).append(output);
 }
 
@@ -81,7 +80,7 @@ function viewAnswerCheckbox(answerVars, questionId, prefix) {
     $.each(answerVars, function (key, val) {
         var input = "<input type='checkbox' name='" + questionId + "' value='" + val.name + "'>";
         var output = "<div class='checkbox'><label>" + input + val.name + " </label></div>";
-        $("#form" + prefix + questionId).append(output);
+        $("#answerDiv" + prefix + questionId).append(output);
     });
 }
 
@@ -89,7 +88,7 @@ function viewAnswerRadio(answerVars, questionId, prefix) {
     $.each(answerVars, function (key, val) {
         var input = "<input type='radio' name='" + questionId + "' value='" + val.name + "'>";
         var output = "<div class='radio'><label>" + input + val.name + " </label></div>";
-        $("#form" + prefix + questionId).append(output);
+        $("#answerDiv" + prefix + questionId).append(output);
     });
 }
 
@@ -101,47 +100,51 @@ function viewAnswerSelect(answerVars, questionId, prefix) {
         output += "<option value='" + val.id + "'>" + val.name + "</option>";
     });
     output += "</select></div>";
-    $("#form" + prefix + questionId).append(output);
+    $("#answerDiv" + prefix + questionId).append(output);
 }
 
 function viewAnswerFree(questionId, prefix) {
     var input = '<span class="input-group-addon"><i class="glyphicon glyphicon-question-sign"></i></span>';
     input += '<textarea class="form-control" rows="2" name="' + questionId + '"></textarea>';
     var output = '<div><div class="input-group">' + input + '</div></div>';
-    $("#form" + prefix + questionId).append(output);
+    $("#answerDiv" + prefix + questionId).append(output);
 }
 
 function viewAnswerName(questionId, prefix) {
     var output = "<div class='input-group'>" + getInputDepartment(questionId, prefix) + "</div>";
     output += "<div class='input-group'>" + getInputGroup(questionId, prefix) + "</div>";
     output += "<div class='input-group'>" + getInputPerson(questionId, prefix) + "</div>";
-    $("#form" + prefix + questionId).append(output);
+    $("#answerDiv" + prefix + questionId).append(output);
     getDepartment(prefix + questionId);
 
 }
 
 function getInputDepartment(questionId, prefix) {
     var inputDepartment = '<span class="input-group-addon"><i class="glyphicon glyphicon-list"></i></span>';
-    inputDepartment += "<select id='department" + prefix + questionId + "' onchange='getGroup(\"" + prefix + questionId + "\")' class='form-control'></select>";
+    var  option = "<option value='нет ответа' selected>сделайте выбор</option>";
+    inputDepartment += "<select id='department" + prefix + questionId + "' onchange='getGroup(\"" + prefix + questionId + "\")' class='form-control'>"+option+"</select>";
     return inputDepartment;
 }
 
 function getInputGroup(questionId, prefix) {
     var inputGroup = '<span class="input-group-addon"><i class="glyphicon glyphicon-list"></i></span>';
-    inputGroup += "<select id='group" + prefix + questionId + "' onchange='getPerson(\"" + prefix + questionId + "\")' class='form-control'></select>";
+    var  option = "<option value='нет ответа' selected>сделайте выбор</option>";
+    inputGroup += "<select id='group" + prefix + questionId + "' onchange='getPerson(\"" + prefix + questionId + "\")' class='form-control'>"+option+"</select>";
     return inputGroup;
 }
 
 function getInputPerson(questionId, prefix) {
+
     var inputPerson = '<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>';
-    inputPerson += "<select id='person" + prefix + questionId + "' name='" + questionId + "' class='form-control'></select>";
+    var  option = "<option value='нет ответа' selected>сделайте выбор</option>";
+    inputPerson += "<select id='person" + prefix + questionId + "' name='" + questionId + "' class='form-control'>"+option+"</select>";
     return inputPerson;
 }
 
 
 function viewOption(data, selectId) {
     $("." + selectId).remove();
-    var output = "<option disabled selected>сделайте выбор</option>";
+    var output = "";
     $.each(data, function (key, val) {
         output += "<option class='" + selectId + "' value='" + val.id + "'>" + val.name + "</option>";
     });
