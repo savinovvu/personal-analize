7d916a35f401fedbff8059c2af822f6338d1ff00
skorpion <savinov_vu@inbox.ru>
2017-03-08 11:28:16 +0800

    add questionnaireMenu.

--- src/main/java/ru/inbox/savinov_vu/controller/page/QuizController.java
+++ src/main/java/ru/inbox/savinov_vu/controller/page/QuizController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import ru.inbox.savinov_vu.model.constructor.question.QuestionKit;
 import ru.inbox.savinov_vu.model.personal.Department;
@@ -32,9 +33,16 @@ public class QuizController {
         return "quiz/survey/survey";
     }
 
+    @GetMapping("questionnaireMenu/{id}")
+    public String getQuestionnaireMenu(HttpServletRequest request, Model model, @PathVariable("id") Integer surveyId) {
+        model.addAttribute("surveyId", surveyId);
+        return "quiz/questionnaireMenu/questionnaireMenu";
+    }
+
+
     @GetMapping("beginNewSurvey")
-    public String beginNewSurvey (HttpServletRequest request,
-                                  RedirectAttributes redirectAttributes, Model model){
+    public String beginNewSurvey(HttpServletRequest request,
+                                 RedirectAttributes redirectAttributes, Model model) {
         LOG.debug("get beginSurveyPage");
 
         Survey survey = new Survey();
@@ -52,7 +60,6 @@ public class QuizController {
     public String continueSurvey(HttpServletRequest request, Model model) {
         int id = getSurveyId(request);
         model.addAttribute("survey_id", id);
-        model.addAttribute("numberOfQuestionnaire", service.getSurveyByID(id).getCount()+1);
         return "quiz/questionnaire/questionnaire";
     }
 
@@ -61,14 +68,14 @@ public class QuizController {
     }
 
     private QuestionKit getSurveyQuestionKit(HttpServletRequest request) {
-        if ("".equals(request.getParameter("questionKit"))){
+        if ("".equals(request.getParameter("questionKit"))) {
             throw new IllegalArgumentException("this parameter must be don't null");
         }
         return new QuestionKit().setId(Integer.valueOf(request.getParameter("questionKit")));
     }
 
     private LocalDate getSurveyDate(HttpServletRequest request) {
-        if ("".equals(request.getParameter("createDate"))){
+        if ("".equals(request.getParameter("createDate"))) {
             return LocalDate.now();
         }
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
@@ -77,21 +84,21 @@ public class QuizController {
 
 
     private Department getSurveyDepartment(HttpServletRequest request) {
-        if (Objects.isNull(request.getParameter("department"))){
+        if (Objects.isNull(request.getParameter("department"))) {
             return null;
         }
         return new Department().setId(Integer.valueOf(request.getParameter("department")));
     }
 
     private Group getSurveyGroup(HttpServletRequest request) {
-        if (Objects.isNull(request.getParameter("group"))){
+        if (Objects.isNull(request.getParameter("group"))) {
             return null;
         }
         return new Group().setId(Integer.valueOf(request.getParameter("group")));
     }
 
     private String getSurveyComment(HttpServletRequest request) {
-             return request.getParameter("comment");
+        return request.getParameter("comment");
     }
 
 
