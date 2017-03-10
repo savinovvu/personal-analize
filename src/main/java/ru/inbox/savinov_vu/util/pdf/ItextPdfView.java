7a30615bcd6e3eab483d0890da95187972e8b7d7
skorpion <savinov_vu@inbox.ru>
2017-03-10 12:26:11 +0800

    Itext with table.

--- src/main/java/ru/inbox/savinov_vu/util/pdf/ItextPdfView.java
+++ src/main/java/ru/inbox/savinov_vu/util/pdf/ItextPdfView.java
@@ -2,6 +2,8 @@ package ru.inbox.savinov_vu.util.pdf;
 
 import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 import ru.inbox.savinov_vu.model.constructor.question.QuestionVar;
 import ru.inbox.savinov_vu.model.quiz.answer.Answer;
@@ -16,10 +18,7 @@ import java.util.*;
 import java.util.List;
 
 
-/**
- * This view class generates a PDF document 'on the fly' based on the data
- * contained in the model.
- */
+
 public class ItextPdfView extends AbstractITextPdfView {
     static final int FONT_SIZE_SMALL = 16;
     static final int FONT_SIZE_BIG = 32;
@@ -55,17 +54,14 @@ public class ItextPdfView extends AbstractITextPdfView {
 
 
         Paragraph methodicals = new Paragraph("Используемая методика: " + survey.getQuestionKit().getName(), fontSmall);
-        methodicals.setSpacingAfter(sizeSpacingAfter);
         document.add(methodicals);
 
         Paragraph dateParagraph = new Paragraph("Дата Тестирования: " + survey.getDate().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")), fontSmall);
-        dateParagraph.setSpacingAfter(sizeSpacingAfter);
         document.add(dateParagraph);
 
         List<Questionnaire> questionnaires = new ArrayList(Arrays.asList(model.get("questionnaires")));
         Paragraph countQuestionnaire = new Paragraph("Количество анкет: " + questionnaires.size(), fontSmall);
-        dateParagraph.setSpacingAfter(sizeSpacingAfter);
-        document.add(dateParagraph);
+        document.add(countQuestionnaire);
 
 
         if (Objects.nonNull(survey.getDepartment())) {
@@ -79,69 +75,40 @@ public class ItextPdfView extends AbstractITextPdfView {
 
         if (Objects.nonNull(survey.getComment()) && !"".equals(survey.getComment())) {
             Paragraph commentParagraph = new Paragraph("комментарий: " + survey.getComment(), fontSmall);
+            commentParagraph.setSpacingAfter(sizeSpacingAfter);
             document.add(commentParagraph);
 
         }
 
 
+        PdfPTable table = new PdfPTable(3);
+        table.setWidths(new int[]{90, 60, 30});
+
+
+        table.addCell(getNormalCell("Вопрос", 15));
+        table.addCell(getNormalCell("Ответ", 15));
+        table.addCell(getNormalCell("Кол-во", 15));
+
         Map<QuestionVar, Map<Answer, Long>> questionAnswerCountMap = (Map<QuestionVar, Map<Answer, Long>>) model.get("countAnswerByQuestionMap");
 
-        for (Map.Entry<QuestionVar, Map<Answer, Long>> questionVarMapEntry : questionAnswerCountMap.entrySet()) {
-            Paragraph questionParagraph = new Paragraph(questionVarMapEntry.getKey().getName(), fontSmall);
-            document.add(questionParagraph);
-            for (Map.Entry<Answer, Long> answerLongEntry : questionVarMapEntry.getValue().entrySet()) {
-                Paragraph answerParagraph = new Paragraph( answerLongEntry.getKey().getName() + " - " + answerLongEntry.getValue(), fontSmall);
-                document.add(answerParagraph);
-            }
 
+        for (Map.Entry<QuestionVar, Map<Answer, Long>> questionVarMapEntry : questionAnswerCountMap.entrySet()) {
+            table.addCell(getNormalCell(questionVarMapEntry.getKey().getName(), 12));
+            table.addCell(getNormalCell("", 12));
+            table.addCell(getNormalCell("", 12));
+            for (Map.Entry<Answer, Long> answerLongEntry : questionVarMapEntry.getValue().entrySet()) {
+                table.addCell(getNormalCell("", 12));
+                table.addCell(getNormalCell(answerLongEntry.getKey().getName(), 12));
+                table.addCell(getNormalCell(String.valueOf(answerLongEntry.getValue()), 12));
+            }
         }
 
-
-/*
-        Paragraph customerNamePr = new Paragraph("Заказчик: " + order.getCustomerName(), fontSmall);
-        customerNamePr.setSpacingAfter(sizeSpacingAfter);
-        document.add(customerNamePr);
-
-        Paragraph customerPhonePr = new Paragraph("Телефон заказчика: " + order.getCustomerPhone(), fontSmall);
-        customerPhonePr.setSpacingAfter(sizeSpacingAfter);
-        document.add(customerPhonePr);
-
-
-        PdfPTable table = new PdfPTable(4);
-        table.setWidths(new int[]{30, 60, 30, 30});
-
-
-        table.addCell(getNormalCell("Артикул",10));
-        table.addCell(getNormalCell("Наименование",60));
-        table.addCell(getNormalCell("Кол-во",30));
-        table.addCell(getNormalCell("Предоплата",30));
-
-        List<Product> products = order.getProducts();
-
-
-
-        for (Product product : products) {
-            table.addCell(getNormalCell(String.valueOf(product.getVendorCode()),10));
-            table.addCell(getNormalCell(product.getProduct_name(),60));
-            table.addCell(getNormalCell(String.valueOf(product.getCountProducts()),30));
-            table.addCell(getNormalCell(String.valueOf(product.getPrepayment()), 30));
-        }*/
-
-    
-/*
-
         document.add(table);
-*/
-
-
-       /* Paragraph comment = new Paragraph("Примечание: " + order.getOrderComment(), fontSmall);
-        customerPhonePr.setSpacingAfter(sizeSpacingAfter);
-        document.add(comment);*/
 
 
     }
 
-/*    private PdfPCell getNormalCell(String string,  float size)
+    private PdfPCell getNormalCell(String string, float size)
             throws DocumentException, IOException {
         if (string != null && "".equals(string)) {
             return new PdfPCell();
@@ -151,12 +118,12 @@ public class ItextPdfView extends AbstractITextPdfView {
             f.setColor(BaseColor.RED);
             size = -size;
         }
-       *//* f.setSize(size);*//*
+        f.setSize(size);
         PdfPCell cell = new PdfPCell(new Phrase(string, f));
         cell.setHorizontalAlignment(Element.ALIGN_CENTER);
         cell.setVerticalAlignment(Element.ALIGN_CENTER);
         return cell;
-    }*/
+    }
 
 
 }
