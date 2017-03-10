18bea2b408252610be328715152076c210eacc90
skorpion <savinov_vu@inbox.ru>
2017-03-10 14:31:43 +0800

    Impelement PDF output result of Quiz.

--- src/main/java/ru/inbox/savinov_vu/util/pdf/ItextPdfView.java
+++ src/main/java/ru/inbox/savinov_vu/util/pdf/ItextPdfView.java
@@ -2,13 +2,13 @@ package ru.inbox.savinov_vu.util.pdf;
 
 import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.BaseFont;
-import com.itextpdf.text.pdf.PdfPCell;
-import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
+import org.springframework.beans.factory.annotation.Autowired;
 import ru.inbox.savinov_vu.model.constructor.question.QuestionVar;
 import ru.inbox.savinov_vu.model.quiz.answer.Answer;
 import ru.inbox.savinov_vu.model.quiz.questionnaire.Questionnaire;
 import ru.inbox.savinov_vu.model.quiz.survey.Survey;
+import ru.inbox.savinov_vu.service.constructor.questionVar.QuestionVarService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,8 +18,11 @@ import java.util.*;
 import java.util.List;
 
 
-
 public class ItextPdfView extends AbstractITextPdfView {
+    @Autowired
+    QuestionVarService service;
+
+
     static final int FONT_SIZE_SMALL = 16;
     static final int FONT_SIZE_BIG = 32;
     static final int OFFSET = 40;
@@ -31,7 +34,7 @@ public class ItextPdfView extends AbstractITextPdfView {
                     BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
 
     final Font fontBig = new Font(timesNewRoman, FONT_SIZE_BIG);
-    final Font fontSmall = new Font(timesNewRoman, 14);
+    final Font font14 = new Font(timesNewRoman, 14);
     final Font font12 = new Font(timesNewRoman, 12);
     final Font font8 = new Font(timesNewRoman, 8);
 
@@ -48,82 +51,71 @@ public class ItextPdfView extends AbstractITextPdfView {
     @Override
     protected void buildPdfDocument(Map<String, Object> model, Document document, PdfWriter writer, HttpServletRequest request, HttpServletResponse response) throws Exception {
         Survey survey = (Survey) model.get("survey");
-        Paragraph title = new Paragraph("Id Тестирования = " + survey.getId(), fontSmall);
+        Paragraph title = new Paragraph("Id Тестирования = " + survey.getId(), font14);
         title.setAlignment(Element.ALIGN_CENTER);
         document.add(title);
 
 
-        Paragraph methodicals = new Paragraph("Используемая методика: " + survey.getQuestionKit().getName(), fontSmall);
+        Paragraph methodicals = new Paragraph("Используемая методика: " + survey.getQuestionKit().getName(), font14);
         document.add(methodicals);
 
-        Paragraph dateParagraph = new Paragraph("Дата Тестирования: " + survey.getDate().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")), fontSmall);
+        Paragraph dateParagraph = new Paragraph("Дата Тестирования: " + survey.getDate().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")), font14);
         document.add(dateParagraph);
 
         List<Questionnaire> questionnaires = new ArrayList(Arrays.asList(model.get("questionnaires")));
-        Paragraph countQuestionnaire = new Paragraph("Количество анкет: " + questionnaires.size(), fontSmall);
+        Paragraph countQuestionnaire = new Paragraph("Количество анкет: " + questionnaires.size(), font14);
         document.add(countQuestionnaire);
 
 
         if (Objects.nonNull(survey.getDepartment())) {
-            Paragraph departmentParagraph = new Paragraph("Подразделение: " + survey.getDepartment().getName(), fontSmall);
+            Paragraph departmentParagraph = new Paragraph("Подразделение: " + survey.getDepartment().getName(), font14);
             document.add(departmentParagraph);
             if (Objects.nonNull(survey.getGroup())) {
-                Paragraph groupParagraph = new Paragraph("Группа: " + survey.getGroup().getName(), fontSmall);
+                Paragraph groupParagraph = new Paragraph("Группа: " + survey.getGroup().getName(), font14);
                 document.add(groupParagraph);
             }
         }
 
         if (Objects.nonNull(survey.getComment()) && !"".equals(survey.getComment())) {
-            Paragraph commentParagraph = new Paragraph("комментарий: " + survey.getComment(), fontSmall);
+            Paragraph commentParagraph = new Paragraph("комментарий: " + survey.getComment(), font14);
             commentParagraph.setSpacingAfter(sizeSpacingAfter);
             document.add(commentParagraph);
 
         }
 
 
-        PdfPTable table = new PdfPTable(3);
-        table.setWidths(new int[]{90, 60, 30});
-
-
-        table.addCell(getNormalCell("Вопрос", 15));
-        table.addCell(getNormalCell("Ответ", 15));
-        table.addCell(getNormalCell("Кол-во", 15));
-
         Map<QuestionVar, Map<Answer, Long>> questionAnswerCountMap = (Map<QuestionVar, Map<Answer, Long>>) model.get("countAnswerByQuestionMap");
 
-
+        QuestionVar savedSuperQuestion = null;
         for (Map.Entry<QuestionVar, Map<Answer, Long>> questionVarMapEntry : questionAnswerCountMap.entrySet()) {
-            table.addCell(getNormalCell(questionVarMapEntry.getKey().getName(), 12));
-            table.addCell(getNormalCell("", 12));
-            table.addCell(getNormalCell("", 12));
-            for (Map.Entry<Answer, Long> answerLongEntry : questionVarMapEntry.getValue().entrySet()) {
-                table.addCell(getNormalCell("", 12));
-                table.addCell(getNormalCell(answerLongEntry.getKey().getName(), 12));
-                table.addCell(getNormalCell(String.valueOf(answerLongEntry.getValue()), 12));
+            QuestionVar question = questionVarMapEntry.getKey();
+            AddSuperQuestion(savedSuperQuestion, question, document);
+
+                Paragraph questionparagraph = new Paragraph(question.getName(), font14);
+                document.add(questionparagraph);
+                for (Map.Entry<Answer, Long> answerLongEntry : questionVarMapEntry.getValue().entrySet()) {
+                    Paragraph answerParagrapg = new Paragraph("                " + answerLongEntry.getKey().getName() + " - " + answerLongEntry.getValue(), font14);
+                    document.add(answerParagrapg);
+                }
+                Paragraph emptyParagraph = new Paragraph(" ");
+                emptyParagraph.setSpacingAfter(sizeSpacingAfter);
+                document.add(emptyParagraph);
+            }
+
+
+        }
+        private void AddSuperQuestion(QuestionVar savedSuperQuestion, QuestionVar currentQuestion , Document document) throws DocumentException {
+
+            if (Objects.nonNull(currentQuestion.getSuperQuestionVarId())) {
+                if (Objects.isNull(savedSuperQuestion) || !savedSuperQuestion.getId().equals(currentQuestion.getSuperQuestionVarId())) {
+                    savedSuperQuestion = service.findById(currentQuestion.getSuperQuestionVarId());
+                    Paragraph questionParagraph = new Paragraph(savedSuperQuestion.getName(), font14);
+                    document.add(questionParagraph);
+
+                }
             }
         }
 
-        document.add(table);
-
-
-    }
-
-    private PdfPCell getNormalCell(String string, float size)
-            throws DocumentException, IOException {
-        if (string != null && "".equals(string)) {
-            return new PdfPCell();
-        }
-        Font f = font12;
-        if (size < 0) {
-            f.setColor(BaseColor.RED);
-            size = -size;
-        }
-        f.setSize(size);
-        PdfPCell cell = new PdfPCell(new Phrase(string, f));
-        cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-        cell.setVerticalAlignment(Element.ALIGN_CENTER);
-        return cell;
     }
 
 
-}
